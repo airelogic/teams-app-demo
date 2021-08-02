565a7fc4b1eb377e04a01d657b4713fb12df03c5
James Ratcliffe <james.ratcliffe@airelogic.com>
2021-08-02 15:12:18 +0100

    Allowed multiple app manifests

--- scripts/make-manifest.ps1
+++ scripts/make-manifest.ps1
@@ -7,10 +7,15 @@ $ErrorActionPreference = "Stop"
 
 Push-Location "$PSScriptRoot/.."
 try {
-    $output = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("artifacts/manifest.zip")
-    New-Item -ItemType Directory -Force artifacts | Out-Null
-    Compress-Archive -Path "eng/manifest/*" -DestinationPath $output -Force
-    Write-Host "Manifest created at $output"
+  New-Item -ItemType Directory -Force artifacts/manifests | Out-Null
+
+  Get-ChildItem -Directory "apps" | ForEach-Object {
+    $appName = $_.BaseName
+
+    $output = $ExecutionContext.SessionState.Path.GetUnresolvedProviderPathFromPSPath("artifacts/manifests/$appName.zip")
+    Compress-Archive -Path "$($_.FullName)/*" -DestinationPath $output -Force
+    Write-Host "$appName manifest created at $output"
+  }
 }
 Finally {
     Pop-Location
