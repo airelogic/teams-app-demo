f89018883efc23d763d9ff2d60c0666a4d1f02af
James Ratcliffe <james.ratcliffe@airelogic.com>
2021-08-02 15:45:23 +0100

    Added more forms

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,18 @@ export default function (): JSX.Element {
             displayName="Consultation Note"
           />
         </Route>
+        <Route path="/utis/config">
+          <NoConfiguration
+            url="https://gm-staging.forms4health.com/gm/singlepass/utis"
+            displayName="Urinary Tract Infection"
+          />
+        </Route>
+        <Route path="/preoperative-questionnaire/config">
+          <NoConfiguration
+            url="https://gm-staging.forms4health.com/gm/singlepass/preoperative-questionnaire"
+            displayName="Preoperative Questionnaire"
+          />
+        </Route>
         <Route path="/">Page not found</Route>
       </Switch>
     </Router>
